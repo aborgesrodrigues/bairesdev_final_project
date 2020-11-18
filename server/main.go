a0aad2316aade7b14328b88a3c5098de7fd2eb64
Alessandro Borges Rodrigues <aborgesrodrigues@gmail.com>
2020-11-18 09:29:57 -0300

    remove enviroment variable

--- server/main.go
+++ server/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"bairesdev_final_project/internal/handler"
 
@@ -16,7 +15,7 @@ func main() {
 	//cmd.CreateDatabase()
 	//cmd.PopulateUser()
 
-	os.Setenv("SQLITEPATH", "/home/alessandro/go/src/bairesdev_final_project/server/sqlite-database.db")
+	//os.Setenv("SQLITEPATH", "/home/alessandro/go/src/bairesdev_final_project/server/sqlite-database.db")
 
 	router := mux.NewRouter().StrictSlash(true)
 	handler.CreateUserRouters(router)
