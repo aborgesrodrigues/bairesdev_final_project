0cf6c6dca0dd321e56d6208606417f7ad6431843
Alessandro Borges Rodrigues <aborgesrodrigues@gmail.com>
2020-11-20 16:20:11 -0300

    included script to populate users

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -23,3 +23,12 @@ CREATE TABLE public.questions (
 -- public.questions foreign keys
 
 ALTER TABLE public.questions ADD CONSTRAINT fk_questions_user FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+
+-- populate users table
+
+INSERT INTO public.users(username, "name", birthday) VALUES('username1', 'User Name 1', '2000-01-01');
+INSERT INTO public.users(username, "name", birthday) VALUES('username2', 'User Name 2', '1995-05-02');
+INSERT INTO public.users(username, "name", birthday) VALUES('username3', 'User Name 3', '2001-07-03');
+INSERT INTO public.users(username, "name", birthday) VALUES('username4', 'User Name 4', '1980-01-10');
+INSERT INTO public.users(username, "name", birthday) VALUES('username5', 'User Name 5', '2005-04-12');
